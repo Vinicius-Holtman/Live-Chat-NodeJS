e10e8c04251cfe69e062b69953237df743a70cce
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-04-21 20:22:15 -0300

    Criei a estrutura do usuario e relacionamentos das Table

---
+++ src/services/UserService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+
+class UsersService {
+    private usersRepository : Repository<User>
+
+    constructor(){
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+    
+    async create(email: string) {
+        // verificar se usuario existe
+
+        const userExists = await this.usersRepository.findOne({
+            email,
+        });
+        // se existir, retornar user
+        if (userExists) {
+            return userExists;
+        }
+
+        const user = this.usersRepository.create({
+            email,
+        });
+
+        await this.usersRepository.save(user);
+
+        // se nao exister, salvar no DB
+        return user;
+    }
+}
+
+export {UsersService};
